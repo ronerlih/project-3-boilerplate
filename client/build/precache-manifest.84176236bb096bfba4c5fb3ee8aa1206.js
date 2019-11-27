46c6a04926cac50e6712cba625fbc14eba782e78
ron erlih <ronerlih@gmail.com>
2019-11-26 21:49:14 -0500

    first commit

---
+++ client/build/precache-manifest.84176236bb096bfba4c5fb3ee8aa1206.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "905c8d1730cdf48a45f7",
+    "url": "/static/js/main.f7c8f1ee.chunk.js"
+  },
+  {
+    "revision": "f2179e7f22e6577b35cf",
+    "url": "/static/js/2.565b7cae.chunk.js"
+  },
+  {
+    "revision": "905c8d1730cdf48a45f7",
+    "url": "/static/css/main.f20fb3c6.chunk.css"
+  },
+  {
+    "revision": "4ac18d236ffea2af6744e440ad88f610",
+    "url": "/index.html"
+  }
+];
